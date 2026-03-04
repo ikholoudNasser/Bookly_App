810c470f8389dc8e6e41db7a32f09cb8aa0f6edf
kholoudnasser <kholoudn03@gmail.com>
2026-03-04 06:40:15 +0200

    first commit

---
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,21 @@
+import 'package:bookly/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Image.asset(AssetsData.logo),
+          SizedBox(height: 4),
+          Text('Read Free Books', textAlign: TextAlign.center),
+        ],
+      ),
+    );
+  }
+}
