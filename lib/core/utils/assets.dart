93faa1c1d1d3908f73299bc9adc532b2f68c416d
kholoudnasser <kholoudn03@gmail.com>
2026-03-08 03:00:48 +0200

    add custom list view item to home

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,3 +1,4 @@
 class AssetsData {
   static const logo = 'assets/images/Logo (1).png';
+  static const test = 'assets/images/test_image.png';
 }
--- lib/features/home/presentation/views/widget/custom_app_bar.dart
+++ lib/features/home/presentation/views/widget/custom_app_bar.dart
@@ -1,5 +1,6 @@
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({super.key});
